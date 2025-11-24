ccea21eed08a50f6bf60bd9356acebf5849af14f
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-11-24 19:28:28 +0100

    Cache request from db

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,12 +1,8 @@
 import Messages from '@/components/messages';
+import { getMessages } from '@/lib/messages';
 
-export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
-  const messages = await response.json();
+export default function MessagesPage() {
+  const messages = getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
